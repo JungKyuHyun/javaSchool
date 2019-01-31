6405c36781188f5759ecfc0a03c8084b1425738a
MJ <MJ>
2019-01-31 15:57:49 +0900

    2019-01-31 데이터 타입 끝

---
+++ src/Ex05_DataType.java
@@ -0,0 +1,25 @@
+
+public class Ex05_DataType {
+	public static void main(String[] args) {
+		// 안에 선언되는 변수 지역변수 (main 함수 안쪽)
+		
+		byte b = 100;
+		System.out.println(b);
+		
+		int i = 12345;
+		System.out.println(i);
+		
+		long l = 10000000000l; // 정수 리터럴은 int
+		
+		float f = 3.14f; // 실수 리터럴은 double
+		
+		double d = 3.141592;
+		
+		boolean bo = true;
+		
+		char ch = 'A';
+		
+		String str = "홍길동";
+	}
+
+}
