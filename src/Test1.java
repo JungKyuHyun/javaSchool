2330d9797e4569512fa2ef023dfd28ce0fec1f5c
MJ <MJ>
2019-02-01 10:54:28 +0900

    aa

--- src/Test1.java
+++ src/Test1.java
@@ -2,6 +2,7 @@ import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class Test1 {
+	
 	public static void main(String[] args) {
 		Path workingDirectory=Paths.get(".").toAbsolutePath();
 		
