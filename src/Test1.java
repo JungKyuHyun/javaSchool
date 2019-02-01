1f0cfb406a50a6d9a51fd98260a450098db85207
MJ <MJ>
2019-02-01 11:59:01 +0900

    2019-02-01 add current path

--- src/Test1.java
+++ src/Test1.java
@@ -1,11 +1,11 @@
 import java.nio.file.Path;
 import java.nio.file.Paths;
 
+// 현재 경로 추가하기 
+
 public class Test1 {
-	
 	public static void main(String[] args) {
 		Path workingDirectory=Paths.get(".").toAbsolutePath();
-		
 		
 		System.out.println(workingDirectory);
 	}
